426b3d02db975ded9205e04335bd596052d67b5e
Daniella Freese <dfreese@localhost.localdomain>
2021-10-22 16:28:43 -0400

    working html is ready

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,11 +12,20 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
+
+	//all middleware pieces
 	mux.Use(middleware.Recoverer)
 	mux.Use(WriteToConsole)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
+
+	//handlers
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	//static images
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
